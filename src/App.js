d2fdb3bdd22b4911e4e2202abeb9fcfae9be887e
olga.olegovna.603 <olga.olegovna.603@list.ru>
2024-06-10 13:48:48 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { data } from "./data";
+import { CardItem } from "./CardItem";
 
 
 
 export const App = () => {
-    return <div>hello world!</div>
-}
+    const [person]=data;
+    const {name,age,phone}=person;
+    return<>
+    <div>hello world!</div>
+    {/* <CardItem name={person.name} age={person.age} phone={person.phone}/> */}
+    {/* <CardItem name={name} age={age} phone={phone}/> */}
+    {/* <CardItem {...person}/>
+    {[<CardItem {...person}/>,<CardItem {...person}/>,<CardItem {...person}/>]} */}
+    {/* {data.map((person)=>{
+        return<CardItem{...person}/>; */}
+    {data.map((person)=><CardItem{...person}/>)}
+    </>
+};
---
+++ src/CardItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const CardItem=({name,age,phone})=>{
+    return(
+        <div>
+            <h2>{name}</h2>
+            <p>{age}</p>
+            <p>{phone}</p>
+        </div>
+    )
+}
